f116f9184296fc04d350386bb39e49b51f506886
YanaKalashnikova <kalashnikova.yana3@gmail.com>
2019-03-23 22:46:39 +0300

    feat: add function which check that for given brackets sequence will return true if it is correct and false otherwise

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  var arr = str.split('');
+  var result = true;
+  while (arr.length && result) {
+    var a = -1;
+    for (var i=0; i<arr.length-1; i++) {
+      for (var j=0; j<bracketsConfig.length; j++) {
+        if (arr[i] === bracketsConfig [j][0] && arr[i+1] === bracketsConfig [j][1] ) {
+          a = i;
+        }
+      }  
+    }
+    if (a === -1) {
+      result = false;
+    } else {
+      arr.splice (a, 2);
+    }
+  }
+  return result;
 }
